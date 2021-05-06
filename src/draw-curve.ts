d17fc7197b5ca8f0230fea405e6928ee547e708e
yutoogi <jacoyutorius@gmail.com>
2021-05-06 21:25:54 +0900

    minorchange draw-curve

--- src/draw-curve.ts
+++ src/draw-curve.ts
@@ -10,12 +10,16 @@ const sketch = (p: p5) => {
   let theta = 0;
  
   p.setup = () => {
-    p.createCanvas(600, 200);
-    centY = p.height / 2;
-    p.background(230);
-    p.strokeWeight(1);
-    p.stroke(255);
-    p.line(0, 100, 600, 100);
+    p.createCanvas(innerWidth, innerHeight);
+    centY = innerHeight / 2;
+    p.background(1);
+    // p.strokeWeight(1);
+    // p.stroke(111);
+    // p.line(0, centY, innerWidth, centY);
+
+    p.frameRate(10);
+
+    console.log({ centY });
   }
 
   p.draw = () => {
@@ -23,12 +27,22 @@ const sketch = (p: p5) => {
   }
 
   function drawCurve() {
-    p.strokeWeight(2);
-    p.stroke(0);
+    p.strokeWeight(1);
+    p.stroke(255);
     x++;
-    y = centY + r * p.sin(p.radians(theta));
+    y = centY + r * p.sin(p.radians(theta)) * 5;
+    p.point(x, centY);
+
+    p.strokeWeight(0.5);
+    p.stroke(p.color(p.random(255), p.random(255), p.random(255)));
+    p.line(x, centY, x, y);
+
     theta += 2;
-    p.point(x, y);
+    // p.fill(1);
+    // p.textSize(50);
+    // p.stroke(255);
+    // p.text(`${x},${y}`, 20, 60);
+    // console.log(`${y}`);
   }
 }
 
